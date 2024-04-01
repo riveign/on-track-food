a1644a4bc2d4fd1f4aa0cbd63e6c81a0ce83641a
Ignacio Rivera Axmann <riveign@gmail.com>
2024-04-01 19:28:46 +0200

    model test

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,9 @@ class Product < ApplicationRecord
   validates :name, presence: true
   validates :category, presence: true
   validates :category, inclusion: { in: %w(Pantry Fridge) }
+
+  def self.categories
+    ['Pantry', 'Fridge']
+  end
+
 end
